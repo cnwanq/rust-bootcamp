26e18c630a8082053e63150cf3f7734bb3f909df
qijian <wanqijian@outlook.com>
2024-11-15 22:58:29 +0800

    rcli support yaml file

--- rcli/src/process.rs
+++ rcli/src/process.rs
@@ -1,7 +1,10 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Player {
@@ -17,15 +20,21 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input).unwrap();
-    let mut res = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result.unwrap();
-        res.push(record);
+    let mut ret = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
 
-    let json = serde_json::to_string_pretty(&res)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+    fs::write(output, content)?;
+
     Ok(())
 }
